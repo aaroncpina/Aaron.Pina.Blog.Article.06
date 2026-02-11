dfc1f3341b105037d94a762f3bf7262e63ee7b03
Aaron Pina <aaron.pina@gmail.com>
2026-02-11 10:26:05 +0100

    Modified the client login endpoint to send POST requests to the server token endpoint and implemented the admin blacklist endpoint.

--- Aaron.Pina.Blog.Article.06.Client/Program.cs
+++ Aaron.Pina.Blog.Article.06.Client/Program.cs
@@ -1,6 +1,8 @@
 using Aaron.Pina.Blog.Article._06.Shared.Responses;
+using Aaron.Pina.Blog.Article._06.Shared.Requests;
 using Aaron.Pina.Blog.Article._06.Client;
 using Aaron.Pina.Blog.Article._06.Shared;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,15 +20,16 @@ foreach (var role in Roles.ValidRoles)
 
 var app = builder.Build();
 
-app.MapGet("{role}/login", async (IHttpClientFactory factory, TokenRepository repository, string role) =>
+app.MapGet("/{role}/login", async (IHttpClientFactory factory, TokenRepository repository, string role) =>
 {
     if (!Roles.ValidRoles.Contains(role)) return Results.BadRequest("Invalid role");
-    var client = factory.CreateClient($"{role}-server-api");
-    using var registerResponse = await client.GetAsync("/register");
+    using var client = factory.CreateClient($"{role}-server-api");
+    using var registerResponse = await client.GetAsync($"{role}/register");
     if (!registerResponse.IsSuccessStatusCode) return Results.BadRequest("Unable to register");
     var userId = await registerResponse.Content.ReadFromJsonAsync<Guid>();
     if (userId == Guid.Empty) return Results.BadRequest("Unable to parse user id");
-    using var tokenResponse = await client.GetAsync($"/token?userId={userId}");
+    var userRequest = new UserRequest(userId, role);
+    using var tokenResponse = await client.PostAsJsonAsync("/token", userRequest);
     if (!tokenResponse.IsSuccessStatusCode) return Results.BadRequest("Unable to get token");
     var token = await tokenResponse.Content.ReadFromJsonAsync<TokenResponse>();
     if (token is null) return Results.BadRequest("Unable to parse token");
@@ -37,10 +40,10 @@ app.MapGet("{role}/login", async (IHttpClientFactory factory, TokenRepository re
     return Results.Ok("Logged in");
 });
 
-app.MapGet("{role}/info", async (IHttpClientFactory factory, TokenRepository repository, string role) =>
+app.MapGet("/{role}/info", async (IHttpClientFactory factory, TokenRepository repository, string role) =>
 {
     if (!Roles.ValidRoles.Contains(role)) return Results.BadRequest("Invalid role");
-    var client = factory.CreateClient($"{role}-server-api");
+    using var client = factory.CreateClient($"{role}-server-api");
     if (client.BaseAddress is null) return Results.BadRequest("Unable to get base address");
     var uriBuilder = new UriBuilder(client.BaseAddress) { Path = "user" };
     using var request = new HttpRequestMessage(HttpMethod.Get, uriBuilder.Uri);
@@ -51,6 +54,22 @@ app.MapGet("{role}/info", async (IHttpClientFactory factory, TokenRepository rep
     var user = await response.Content.ReadFromJsonAsync<UserResponse>();
     if (user is null) return Results.BadRequest("Unable to parse user info");
     return Results.Ok($"User Id: {user.UserId}");
+});
+
+app.MapGet("/admin/blacklist", async (IHttpClientFactory factory, TokenRepository repository) =>
+{
+    var store = repository.TokenStores["admin"];
+    if (store.AccessTokenExpiresAt is null) return Results.BadRequest("Admin token not yet initialised");
+    var handler = new JwtSecurityTokenHandler();
+    if (!handler.CanReadToken(store.AccessToken)) return Results.BadRequest("Invalid access token");
+    var token = handler.ReadJwtToken(store.AccessToken);
+    var claim = token.Claims.FirstOrDefault(c => c.Type == "jti");
+    if (claim is null || !Guid.TryParse(claim.Value, out var jti)) return Results.BadRequest("No jti claim found");
+    var request = new BlacklistRequest(jti, store.AccessTokenExpiresAt.Value);
+    using var client = factory.CreateClient("admin-server-api");
+    var response = await client.PostAsJsonAsync("/blacklist", request);
+    if (!response.IsSuccessStatusCode) return Results.BadRequest("Unable to blacklist token");
+    return Results.Ok("Token blacklisted");
 });
 
 app.Run();
